f4256e105a4fc1613417875424840d7eb313dcd4
김선민 <tjsals116@gmail.com>
2022-07-06 23:16:27 +0900

    organized webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,31 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  devtool: "inline-source-map",
-
+  entry: "./src/index.js" /** 실행의 시작점 */,
+  mode: "development" /** 개발환경별로 webpack의 내장기능 사용가능합니다. @Link https://webpack.kr/configuration/mode/#root */,
+  devtool:
+    "inline-source-map" /** devtool 옵션은 소스맵 생성 여부와 방법을 제어합니다. @Link https://webpack.kr/configuration/devtool/ */,
   devServer: {
-    static: "./dist",
+    static: path.resolve(
+      __dirname,
+      "dist"
+    ) /** 개발서버관련 설정 @Link https://webpack.kr/configuration/dev-server/#root */,
   },
-  mode: "development",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-      title: "Development",
-    }),
-  ],
+  optimization: {
+    runtimeChunk: "single",
+  } /** @Link https://webpack.kr/configuration/optimization/#root */,
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "assets/[hash][ext][query]",
-  },
+  } /** webpack의 build 결과물에 대한 설정 @Link https://webpack.kr/configuration/output/#root */,
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "from webpack",
+    }),
+  ] /** @Link https://webpack.kr/plugins/#root */,
   module: {
+    /** @Link https://webpack.kr/configuration/module/#root */
     rules: [
       {
         test: /\.css$/i,
@@ -35,8 +41,5 @@ module.exports = {
         type: "asset/resource",
       },
     ],
-  },
-  optimization: {
-    runtimeChunk: "single",
   },
 };
