5fa7ba4f3ecab86495db082dfaec40b0badf92ca
김선민 <tjsals116@gmail.com>
2022-07-13 13:19:08 +0900

    rollup plugin-html 설치

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,22 @@
-import { terser } from "rollup-plugin-terser";
 import { resolve } from "path";
+
+import { terser } from "rollup-plugin-terser"; /** @Link https://www.npmjs.com/package/rollup-plugin-terser */
+import html from "@rollup/plugin-html"; /** @Link https://www.npmjs.com/package/@rollup/plugin-html */
+
+
+/**
+ * @Link https://github.com/rollup/plugins/tree/master/packages/html#options
+ */
+const HTML_OPTIONS = {
+  attributes: {
+    html: {
+      lang: "ko",
+    },
+    link: null,
+    script: null,
+  },
+  title: "Hello, Rollup",
+};
 
 export default {
   input: resolve(__dirname, "src/index.js") /** 실행의 시작점 */,
@@ -12,11 +29,12 @@ export default {
       file: resolve(__dirname, "dist/bundle.min.js"),
       format: "iife",
       name: "version",
-      plugins: [terser()],
+      plugins: [html(HTML_OPTIONS), terser()],
     },
   ],
 };
 
 /**
+ * Good to note
  * @Link build-hooks: https://rollupjs.org/guide/en/#build-hooks
  */
