e6719680d37f3b003e7d23e9bc7ae273f9a8679b
김선민 <tjsals116@gmail.com>
2022-07-13 13:19:25 +0900

    snowpack 설치

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mode: "development",
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    out: "dist/bundle.js",
+    /* ... */
+  },
+};
