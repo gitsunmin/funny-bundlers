7ceafb71cb7347524bc94ed38e8f0a27f455ad4a
김선민 <tjsals116@gmail.com>
2023-07-21 00:51:24 +0900

    added rollup, esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,15 @@ import { resolve } from "path";
 import { terser } from "rollup-plugin-terser"; /** @Link https://www.npmjs.com/package/rollup-plugin-terser */
 import html from "@rollup/plugin-html"; /** @Link https://www.npmjs.com/package/@rollup/plugin-html */
 
+const OUTPUT_PATH = "rollup_dist";
 
 /**
  * @Link https://github.com/rollup/plugins/tree/master/packages/html#options
  */
 const HTML_OPTIONS = {
+  fileName: "index.html",
+  meta: [],
+  publicPath: "",
   attributes: {
     html: {
       lang: "ko",
@@ -22,11 +26,11 @@ export default {
   input: resolve(__dirname, "src/index.js") /** 실행의 시작점 */,
   output: [
     {
-      file: resolve(__dirname, "dist/index.js"),
+      file: resolve(__dirname, `${OUTPUT_PATH}/index.js`),
       format: "cjs",
     },
     {
-      file: resolve(__dirname, "dist/index.min.js"),
+      file: resolve(__dirname, `${OUTPUT_PATH}/index.min.js`),
       format: "iife",
       name: "version",
       plugins: [html(HTML_OPTIONS), terser()],
